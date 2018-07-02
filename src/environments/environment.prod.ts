545a8727a5df110f25b6b59e02f323410164ad9b
ericksonqc1999 <ericksonraulquispechurata1999@gmail.com>
2018-07-02 09:20:49 -0500

    v-1.0.0

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCtqsqO8r05PJTYuqxWozOa31RKtSSh1tg",
+    authDomain: "my-app-crud.firebaseapp.com",
+    databaseURL: "https://my-app-crud.firebaseio.com",
+    projectId: "my-app-crud",
+    storageBucket: "my-app-crud.appspot.com",
+    messagingSenderId: "930024886018"
+  }
+};
